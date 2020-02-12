ae63f58821d5efeb7fc3cd94ebf3fbbc9ea7c1f7
romanver <romanver777@yandex.ru>
2020-02-12 18:43:40 +0300

    код разбит на модули

--- js/setup.js
+++ js/setup.js
@@ -1,143 +1,111 @@
+(function() {
 
-const setupEl = document.querySelector('.setup');
-const setupOpen = document.querySelector('.setup-open');
-const setupClose = document.querySelector('.setup-close');
+    const setupEl = document.querySelector('.setup');
+    const setupOpen = document.querySelector('.setup-open');
+    const setupClose = document.querySelector('.setup-close');
 
-const similarHero = [];
+    const similarHero = [];
 
-const fNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Юлия', 'Люпита', 'Вашингтон', 'Виктор'];
-const sNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-const coatColors = ['rgb(101, 131, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-const eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-const fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+    const fNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Юлия', 'Люпита', 'Вашингтон', 'Виктор'];
+    const sNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+    const coatColors = ['rgb(101, 131, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+    const eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+    const fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 // генерация случайного числа
-function getRandomNumber(min, max) {
+    function getRandomNumber(min, max) {
 
-  return Math.floor(Math.random() * (max - min) + min);
-}
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
 // создание массива случайных неповторяющихся чисел
-function getArrayRandomIndexes(array, count) {
-  const indexes = new Set();
+    function getArrayRandomIndexes(array, count) {
+        const indexes = new Set();
 
-  while (indexes.size < count) {
-    indexes.add(Math.floor(Math.random() * Math.floor(array.length)));
-  }
+        while (indexes.size < count) {
+            indexes.add(Math.floor(Math.random() * Math.floor(array.length)));
+        }
 
-  return Array.from(indexes);
-}
+        return Array.from(indexes);
+    }
+
 // создание полного имени
-function getFullName(firstNames, secondNames) {
-  const fullName = [];
-  const arrIndexes = getArrayRandomIndexes(firstNames, 4);
+    function getFullName(firstNames, secondNames) {
+        const fullName = [];
+        const arrIndexes = getArrayRandomIndexes(firstNames, 4);
 
-  for (const i of arrIndexes) {
-    fullName.push(`${firstNames[i]} ${secondNames[i]} `);
-  }
+        for (const i of arrIndexes) {
+            fullName.push(`${firstNames[i]} ${secondNames[i]} `);
+        }
 
-  return fullName;
-}
+        return fullName;
+    }
+
 // заполнение массива волшебников
-function fillArrayHeroes(names, colors, eyes) {
-  const indColors = getArrayRandomIndexes(colors, 4);
-  const indEyes = getArrayRandomIndexes(eyes, 4);
+    function fillArrayHeroes(names, colors, eyes) {
+        const indColors = getArrayRandomIndexes(colors, 4);
+        const indEyes = getArrayRandomIndexes(eyes, 4);
 
-  for (let i = 0; i < names.length; i++) {
-    similarHero.push({
-      name: names[i],
-      coatColor: colors[indColors[i]],
-      eyesColor: eyes[indEyes[i]],
-    });
-  }
-}
+        for (let i = 0; i < names.length; i++) {
+            similarHero.push({
+                name: names[i],
+                coatColor: colors[indColors[i]],
+                eyesColor: eyes[indEyes[i]],
+            });
+        }
+    }
+
 // вывод волшебников в DOM
-function addSimilarWizards(wizards) {
-  const template = document.querySelector('#similar-wizard-template').content.querySelector('div');
-  const setupDiv = document.querySelector('.setup-similar-list');
+    function addSimilarWizards(wizards) {
+        const template = document.querySelector('#similar-wizard-template').content.querySelector('div');
+        const setupDiv = document.querySelector('.setup-similar-list');
 
-  for (const el of wizards) {
-    const element = template.cloneNode(true);
+        for (const el of wizards) {
+            const element = template.cloneNode(true);
 
-    element.querySelector('.setup-similar-label').innerText = el.name;
-    element.querySelector('.wizard-coat').setAttribute('fill', el.coatColor);
-    element.querySelector('.wizard-eyes').setAttribute('fill', el.eyesColor);
+            element.querySelector('.setup-similar-label').innerText = el.name;
+            element.querySelector('.wizard-coat').setAttribute('fill', el.coatColor);
+            element.querySelector('.wizard-eyes').setAttribute('fill', el.eyesColor);
 
-    setupDiv.appendChild(element);
-  }
+            setupDiv.appendChild(element);
+        }
 
-  document.querySelector('.setup-similar').classList.remove('hidden');
-}
+        document.querySelector('.setup-similar').classList.remove('hidden');
+    }
 
 // открытие-закрытие окна настройки персонажа
-setupOpen.addEventListener('click', () => setupEl.classList.remove('hidden'));
-setupClose.addEventListener('click', () => setupEl.classList.add('hidden'));
+    setupOpen.addEventListener('click', () => setupEl.classList.remove('hidden'));
+    setupClose.addEventListener('click', () => {
+
+        setupEl.classList.add('hidden');
+        setupEl.style = '';
+    });
 
 // замена цвета мантии при клике
-const setupWizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+    const setupWizardCoat = document.querySelector('.setup-wizard .wizard-coat');
 
-setupWizardCoat.addEventListener('click', () => {
+    setupWizardCoat.addEventListener('click', () => {
 
-  setupWizardCoat.setAttribute('style', `fill: ${coatColors[getRandomNumber(0, coatColors.length)]}`);
-});
+        setupWizardCoat.setAttribute('style', `fill: ${coatColors[getRandomNumber(0, coatColors.length)]}`);
+    });
 
 // замена цвета глаз при клике
-const setupWizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+    const setupWizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
 
-setupWizardEyes.addEventListener('click', () => {
+    setupWizardEyes.addEventListener('click', () => {
 
-  setupWizardEyes.setAttribute('style', `fill: ${eyesColors[getRandomNumber(0, eyesColors.length)]}`);
-});
+        setupWizardEyes.setAttribute('style', `fill: ${eyesColors[getRandomNumber(0, eyesColors.length)]}`);
+    });
 
 // замена цвета файрбола при клике
-const setupFireballWrap = document.querySelector('.setup-fireball-wrap');
+    const setupFireballWrap = document.querySelector('.setup-fireball-wrap');
 
-setupFireballWrap.addEventListener('click', () => {
+    setupFireballWrap.addEventListener('click', () => {
 
-    setupFireballWrap.setAttribute('style', `background: ${fireballColors[getRandomNumber(0, fireballColors.length)]}`);
-});
+        setupFireballWrap.setAttribute('style', `background: ${fireballColors[getRandomNumber(0, fireballColors.length)]}`);
+    });
 
+    fillArrayHeroes(getFullName(fNames, sNames), coatColors, eyesColors);
+    addSimilarWizards(similarHero);
 
-// перетаскивание окна персонажа
-let setupUserPic = setupEl.querySelector('.setup-user-pic');
-
-setupUserPic.addEventListener('mousedown', (event) => {
-
-    event.preventDefault();
-
-    let startCoords = {
-        x: event.clientX,
-        y: event.clientY
-    };
-
-    let onMouseMove = (moveEvent) => {
-
-        moveEvent.preventDefault();
-
-        let shift = {
-            x: startCoords.x - moveEvent.clientX,
-            y: startCoords.y - moveEvent.clientY
-        };
-
-        startCoords = {
-            x: moveEvent.clientX,
-            y: moveEvent.clientY
-        };
-
-        setupEl.style.left = `${setupEl.offsetLeft - shift.x}px`;
-        setupEl.style.top = `${setupEl.offsetTop - shift.y}px`;
-    };
-    let onMouseUp = (upEvent) => {
-
-        upEvent.preventDefault();
-
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-
-});
-
-fillArrayHeroes(getFullName(fNames, sNames), coatColors, eyesColors);
-addSimilarWizards(similarHero);
+})();
